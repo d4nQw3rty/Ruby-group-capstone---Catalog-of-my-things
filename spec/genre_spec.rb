e9b4808b7219d225c363c4b73c8f92fba201d2f3
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-11 01:22:41 +0300

    Update unit test

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require_relative '../genre'
-require_relative '../music_Album'
+require_relative '../music_album'
 
 describe Genre do
   it 'creates a song using the provided data' do
@@ -8,12 +8,5 @@ describe Genre do
     expect(genre.name).to eql('Genre')
     expect(genre.id).to be_a(Integer)
     expect(genre.items).to be_a(Array)
-  end
-end
-
-describe '#add_item' do
-  it 'add items to the array and genre to the item properties' do
-    genre = Genre.new('Genre')
-    music = MusicAlbum.new('name', '2020-10-12')
   end
 end
