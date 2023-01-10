d4ea6f1e8696e48910f9db19ad46ea73a93e5d22
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-10 19:42:53 +0300

    Add methods to the musicAlbum class

--- musicAlbum.rb
+++ musicAlbum.rb
@@ -1,3 +1,18 @@
-class Music
+require './item'
 
+class MusicAlbum < Item
+  def initialize(on_spotify: true)
+    super(on_spotify)
+    @on_spotify = on_spotify
+  end
+
+  attr_reader :on_spotify
+
+  def can_be_archived?
+    if @on_spotify == true
+      true
+    else
+      false
+    end
+  end
 end
