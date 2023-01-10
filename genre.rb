4d35c8a57cfac47b0d0f284d29c7df3a8a6e59db
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-10 23:57:38 +0300

    Update the add_item method

--- genre.rb
+++ genre.rb
@@ -1,13 +1,14 @@
-class Genre 
+class Genre
   attr_reader :name, :id, :items
+
   def initialize(name, id: Random.rand(1..1000))
     @name = name
     @id = id
-    @item = []
+    @items = []
   end
-  
+
   def add_item(item)
-    @item.push(item)
+    @items.push(item)
     item.genre = self
   end
 end
