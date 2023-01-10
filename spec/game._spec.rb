5dbade2a65bd0513795d76d70343afde837cef70
d4nQw3rty <danlinmoy@gmail.com>
2023-01-10 11:57:34 -0400

    fix linters

--- spec/game._spec.rb
+++ spec/game._spec.rb
@@ -2,12 +2,12 @@ require 'rspec'
 require_relative '../game'
 
 describe Game do
-  it "should be able to be archived" do
+  it 'should be able to be archived' do
     game = Game.new('Super Mario', '1990-01-01', true, '2010-01-01')
     expect(game.can_be_archived?).to be true
   end
 
-  it "should not be able to be archived" do
+  it 'should not be able to be archived' do
     game = Game.new('Super Mario', '1990-01-01', true, '2022-01-01')
     expect(game.can_be_archived?).to be false
   end
