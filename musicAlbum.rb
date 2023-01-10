49e94e47ee92325fe0b0115f45f44225ede38ded
Kelvin-Ben <[kevoben14@gmail.com]>
2023-01-10 22:41:33 +0300

    Update can_be_archive method

--- musicAlbum.rb
+++ musicAlbum.rb
@@ -8,12 +8,13 @@ class MusicAlbum < Item
     @on_spotify = on_spotify
   end
 
-
   def can_be_archived?
-    if @on_spotify == true
+    if @on_spotify == true && super == true
       true
     else
       false
     end
   end
 end
+album = MusicAlbum.new('name', '2000-01-01', on_spotify: true)
+puts album.can_be_archived?
