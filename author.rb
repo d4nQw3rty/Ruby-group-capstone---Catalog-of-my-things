e274da6d3c5156ce868a6a053ce72942e891b48d
d4nQw3rty <danlinmoy@gmail.com>
2023-01-11 11:00:30 -0400

    Update author.rb

--- author.rb
+++ author.rb
@@ -1,10 +1,10 @@
 class Author
-  attr_accessor :first_name, :last_name, :id, :items
+  attr_accessor :id, :first_name, :last_name,  :items
 
   def initialize(first_name, last_name, id: Random.rand(1..1000))
-    @first_name = first_name
-    @last_name = last_name
     @id = id
+    @first_name = first_name
+    @last_name = last_name    
     @items = []
   end
 
